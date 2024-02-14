231fd561b674c55df8b72c7756828ffd6aeed2f7
Ossi Lahti <48803845+ossilahti@users.noreply.github.com>
2024-02-14 14:12:56 +0200

    UI draft with mocked data (#9)
    * frontend: update UI with mock data
    
    * Move dist/ back to ignores and run lint
    
    * Fix tests and pipeline errors
    
    * Change netlify to deploy only when PR is done to prod-branch

--- src/pages/hi/[name].vue
+++ src/pages/hi/[name].vue
@@ -1,12 +1,6 @@
 <script setup lang="ts">
 const router = useRouter()
-const route = useRoute('/hi/[name]')
-const user = useUserStore()
 const { t } = useI18n()
-
-watchEffect(() => {
-  user.setNewName(route.params.name)
-})
 </script>
 
 <template>
@@ -14,26 +8,10 @@ watchEffect(() => {
     <div text-4xl>
       <div i-carbon-pedestrian inline-block />
     </div>
-    <p>
-      {{ t('intro.hi', { name: user.savedName }) }}
-    </p>
 
     <p text-sm opacity-75>
       <em>{{ t('intro.dynamic-route') }}</em>
     </p>
-
-    <template v-if="user.otherNames.length">
-      <p mt-4 text-sm>
-        <span opacity-75>{{ t('intro.aka') }}:</span>
-        <ul>
-          <li v-for="otherName in user.otherNames" :key="otherName">
-            <RouterLink :to="`/hi/${otherName}`" replace>
-              {{ otherName }}
-            </RouterLink>
-          </li>
-        </ul>
-      </p>
-    </template>
 
     <div>
       <button
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,50 +2,126 @@
 defineOptions({
   name: 'IndexPage',
 })
+
 const user = useUserStore()
-const name = ref(user.savedName)
-
-const router = useRouter()
-function go() {
-  if (name.value)
-    router.push(`/hi/${encodeURIComponent(name.value)}`)
-}
-
+const amount = ref(user.amount)
+const price = ref(user.price)
+const bidSelected = ref(user.bidSelected)
+const offerSelected = ref(user.offerSelected)
 const { t } = useI18n()
+
+function cancel() {
+  bidSelected.value = false
+  offerSelected.value = false
+  price.value = 0
+  amount.value = 0
+}
 </script>
 
 <template>
   <div>
     <div text-4xl>
-      <div i-carbon-campsite inline-block />
+      <div i-carbon-finance inline-block />
     </div>
     <p>
       <a rel="noreferrer" href="https://github.com/antfu/vitesse" target="_blank">
-        Vitesse
+        Brokerage ABC
       </a>
     </p>
     <p>
       <em text-sm opacity-75>{{ t('intro.desc') }}</em>
     </p>
 
-    <div py-4 />
+    <hr ml-auto mr-auto mt-6 w-100>
 
-    <TheInput
-      v-model="name"
-      :placeholder="t('intro.whats-your-name')"
-      autocomplete="false"
-      @keydown.enter="go"
-    />
-    <label class="hidden" for="input">{{ t('intro.whats-your-name') }}</label>
+    <p mb-2 mt-6>
+      <!-- This should be done on backend to fetch the stock price -->
+      <em text-sm>Latest traded price: <b>xxx.xx</b> (fetched hourly)</em>
+    </p>
 
     <div>
       <button
-        m-3 text-sm btn
-        :disabled="!name"
-        @click="go"
+        style="width: 15%"
+        m-3 text-sm bid-btn
+        @click="bidSelected = !bidSelected"
       >
-        {{ t('button.go') }}
+        {{ t('button.bid') }}
       </button>
+
+      <button
+        style="width: 15%"
+        m-3 text-sm offer-btn
+        :disabled="bidSelected"
+        @click="offerSelected = !offerSelected"
+      >
+        {{ t('button.offer') }}
+      </button>
+    </div>
+
+    <fieldset m-a w-xl border border-rd>
+      <legend>{{ bidSelected && !offerSelected ? 'Offer' : 'Bid' }}</legend>
+      <div w-full flex justify-center flex-items-center>
+        <div flex flex-col flex-items-start>
+          <em>{{ t(`intro.amount`) }}</em>
+          <TheInput
+            v-model="amount"
+            :placeholder="t('intro.amount')"
+            autocomplete="false"
+          />
+        </div>
+
+        <div flex flex-col flex-items-start style="margin-left: 22px;">
+          <em>{{ t(`intro.price`) }}</em>
+          <TheInput
+            v-model="price"
+            :placeholder="t('intro.price')"
+            autocomplete="false"
+          />
+        </div>
+      </div>
+
+      <p mb-2 mt-4>
+        <em text-sm>Total: {{ amount && price ? amount * price : 0 }} USD</em>
+      </p>
+    </fieldset>
+
+    <div>
+      <button
+        style="width: 15%"
+        m-3 text-sm btn-default
+        @click="cancel"
+      >
+        {{ t('button.cancel') }}
+      </button>
+
+      <button
+        style="width: 15%"
+        m-3 text-sm btn-default
+        :disabled="!price && !amount"
+        data-test-id="confirm-button"
+      >
+        {{ t('button.confirm') }}
+      </button>
+    </div>
+
+    <!-- Mock data for bids and offers to show on UI -->
+    <div flex-column m-auto mt-10 w-75 flex justify-center>
+      <div>
+        <b color-bluegray-800>Bids</b>
+        <ul>
+          <li>40 @ 102.8</li>
+          <li>5 @ 101.4</li>
+          <li>100 @ 101.2</li>
+        </ul>
+      </div>
+      <div ml-12>
+        <b color-bluegray-800>Offers</b>
+        <ul>
+          <li>250 @ 102.9</li>
+          <li>900 @ 103.1</li>
+          <li>1200 @ 103.2</li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
